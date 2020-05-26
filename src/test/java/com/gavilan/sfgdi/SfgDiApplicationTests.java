7076daaf688170ff7b2819669abfac3ac8a42bef
Eze Gavilán <eze_gavilan@hotmail.com>
2020-05-26 17:16:40 -0300

    Project added.

---
+++ src/test/java/com/gavilan/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavilan.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
